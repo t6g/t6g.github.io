095cc1bc7b84c7caf9b7c28ee43a69882016a724
t6g <guopingtangva@gmail.com>
2021-05-08 11:41:21 -0400

    Redesign Open Channel using jQuery and Bootstrap 5
    Triangular

---
+++ openchannel/js/model/openchannel.js
@@ -0,0 +1,82 @@
+var oc = {
+    isUSCustomary : true,
+    isLightMode: false,
+
+    KuUS : 1.487,
+    KuSI : 1.0,
+    gUS : 32.17,
+    gSI : 9.81,
+    m2ft : 3.28,
+    ft2in : 12,
+    in2mm : 25.4,
+
+    get g () {
+        return this.isUSCustomary ? this.gUS : this.gSI;
+    },
+    
+    get Ku (){
+        return this.isUSCustomary ? this.KuUS : this.KuSI;
+    },
+
+    X : 2.0 / 3.0,
+    Y : 1.0 / 2.0,
+
+    TolD : 0.000001, //tolerance for depth ft or m
+    TolA : 0.000001, //tolerance for angle (radian)
+    TolQ : 0.000001, //tolerance for flow rate, cfs or m^3/s
+    MaxCount: 100,   //maximum number of iterations for Newton's Method
+    
+    offsetLeft : 60,
+    offsetTop : 0,
+    offsetRight : 30,
+    offsetBottom : 60
+};
+
+class OpenChannel {
+    constructor(cs, mN, dn) {
+        this.cs = cs;
+        this.mN = mN;
+        this.dn = dn;
+    }
+
+    //getters
+    get an() {
+        throw new TypeError('area is not defined in OpenChannel');
+    }
+    get pn() {
+        throw new TypeError('peri is not defined in OpenChannel');
+    }
+    get vn() {
+        return oc.Ku / this.mN * Math.pow(this.an / this.pn, oc.X) * Math.pow(this.cs, oc.Y);
+    }
+    get Qn() {
+        return this.vn * this.an;
+    }
+    get ac() {
+        throw new TypeError('vc is not defined in OpenChannel');
+    }
+    get pc() {
+        throw new TypeError('dc is not defined in OpenChannel');
+    }
+    get vc() {
+        throw new TypeError('vc is not defined in OpenChannel');
+    }
+    get dc() {
+        throw new TypeError('dc is not defined in OpenChannel');
+    }
+    get sc() {
+        return Math.pow(this.vc / (oc.Ku / this.mN * Math.pow(this.ac / this.pc, oc.X)), 1.0/oc.Y);
+    }
+    get fr() {
+        return this.vn / this.vc;
+    }
+    get depth() {
+        return Math.max(1.0, Math.max(Math.ceil(this.dn), Math.ceil(this.dc)));
+    }
+    Q2Dn(Q) {
+        throw new TypeError('sc is not defined in OpenChannel');
+    }
+    get isClosedConduit() {
+        return false;
+    }
+}
---
+++ openchannel/js/util.js
@@ -0,0 +1,96 @@
+// Hydraulic Calculator util.js 1.0 2021-02-07 by GT
+
+function hambergerClick(){
+    if(document.getElementById('Tria').style.display === 'none') {
+        document.getElementById('Tria').style.display = 'block';
+        document.getElementById('Rect').style.display = 'block';
+        document.getElementById('Trap').style.display = 'block';
+        document.getElementById('Para').style.display = 'block';
+        document.getElementById('Irre').style.display = 'block';
+        document.getElementById('Circ').style.display = 'block';
+        document.getElementById('Elli').style.display = 'block';
+        document.getElementById('Arch').style.display = 'block';
+        document.getElementById('Sett').style.display = 'block';
+    }
+    else {
+        document.getElementById('Tria').style.display = 'none';
+        document.getElementById('Rect').style.display = 'none';
+        document.getElementById('Trap').style.display = 'none';
+        document.getElementById('Para').style.display = 'none';
+        document.getElementById('Irre').style.display = 'none';
+        document.getElementById('Circ').style.display = 'none';
+        document.getElementById('Elli').style.display = 'none';
+        document.getElementById('Arch').style.display = 'none';
+        document.getElementById('Sett').style.display = 'none';
+    }
+}
+
+function hideRbRtRc()
+{
+    w3.hide('#pathRb');
+    w3.hide('#pathRt');
+    w3.hide('#pathRc');
+    w3.hide('#rtLabel');
+    w3.hide('#rbLabel');
+    w3.hide('#rcLabel');
+}
+
+// calculate increment for x, y axis grids/labels
+function niceIncrement(Min, Max){
+    var maxTicks = 7;
+    var niceRange = niceNum(Max - Min, false);
+    var increment = niceNum(niceRange/(maxTicks - 1), true);
+    return increment;
+}
+
+function niceNum(range, bround) {
+    var powValue = Math.pow(10, Math.floor(Math.log10(range)));
+    var fraction = range / powValue;
+
+    var niceFraction;
+    if (bround) {
+        if (fraction < 1.5)
+        {
+            niceFraction = 1;
+        }
+        else if (fraction < 3)
+        {
+            niceFraction = 2;
+        }
+        else if (fraction < 7)
+        {
+            niceFraction = 5;
+        }
+        else
+        {
+            niceFraction = 10;
+        }
+    }
+    else
+    {
+        if (fraction <= 1)
+        {
+            niceFraction = 1;
+        }
+        else if (fraction <= 2)
+        {
+            niceFraction = 2;
+        }
+        else if (fraction <= 5)
+        {
+            niceFraction = 5;
+        }
+        else
+        {
+            niceFraction = 10;
+        }
+    }
+
+    return niceFraction * powValue;
+}
+
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0; 
+}
+
