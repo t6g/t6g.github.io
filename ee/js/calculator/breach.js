f3112475ccd4f3cf5b1282d6169b2572a767637d
t6g <guopingtangva@gmail.com>
2021-07-13 22:12:23 -0400

    Add Dam breach

---
+++ ee/js/calculator/breach.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+    
+    $("#mynav").load("nav.html");
+    
+    $("#btnSave").click(function() {
+        //localStorage.setItem('breach', JSON.stringify(amount));
+    });
+
+
+
+});
